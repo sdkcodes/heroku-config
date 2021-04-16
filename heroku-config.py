93ae078786c7afdd17c7d3af61d666ae43e33bea
Lesa Wright <lswright@gmail.com>
2021-04-16 10:43:24 +0100

    Update heroku-config.py
    Updated to complete the set command - note the spave before and after -a .

--- heroku-config.py
+++ heroku-config.py
@@ -13,7 +13,7 @@ def splitLineIntoParts(line):
 	return line
 
 def setConfigVar(name, value):
-	os.system('heroku config:set ' + name + '=' + value)
+	os.system('heroku config:set ' + name + '=' + value + ' -a ' + 'targetappname')
 
 with open('.env') as e:
 	
